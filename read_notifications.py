172df59c571e40b464550ecc0c0cdee5a1309a5e
César Jerez <csrstrps@gmail.com>
2024-06-25 19:25:13 -0600

    Temp commit for working in other stuff

--- read_notifications.py
+++ read_notifications.py
@@ -2,67 +2,84 @@ import sqlite3
 import re
 import os
 import csv
+import datetime
 
 
-file_path = os.path.join(os.path.dirname(__file__), 'files', 'WhatsApp Chat with NOTIFICATION.txt')
+file_path = os.path.join(
+    os.path.dirname(__file__), "files", "WhatsApp Chat with NOTIFICATION.txt"
+)
 
-csv_file_path = os.path.join(os.path.dirname(__file__), 'files', 'log.csv')
+csv_file_path = os.path.join(os.path.dirname(__file__), "files", "log.csv")
 
-connection = sqlite3.connect('db.db')
+connection = sqlite3.connect("db.db")
 cursor = connection.cursor()
 
-#clean table log
-cursor.execute('''
-    DELETE FROM log
-''')
-
-#delete table log
+# clean table log
 # cursor.execute('''
-#     DROP TABLE IF EXISTS log
+#     DELETE FROM log
 # ''')
 
-#craete table log if not exists
+# delete table log
+# cursor.execute(
+#     """
+#     DROP TABLE IF EXISTS log
+# """
+# )
 
-cursor.execute('''
+# craete table log if not exists
+
+cursor.execute(
+    """
     CREATE TABLE IF NOT EXISTS log(
+        log_id INTEGER PRIMARY KEY,
         date TEXT,
         time TEXT,
+        timestamp INTEGER,
         user TEXT,
         flag TEXT,
         message TEXT,   
-        datetime INTEGER, 
-        PRIMARY KEY(date, time, user, flag)        
-    )
-''')
+        puch_date TEXT,
+        punch_time TEXT,
+        punch_timestamp INTEGER,
+        is_valid BOOLEAN DEFAULT 1,
+        notes TEXT
+    );    
+"""
+)
+cursor.execute('CREATE UNIQUE INDEX IF NOT EXISTS idx_log ON log (date, time, user, flag);')
 
-cursor.execute('''
+cursor.execute(
+    """
                CREATE TABLE IF NOT EXISTS flag(
                     flag TEXT PRIMARY KEY,
                     description TEXT
                 )   
-''')
+"""
+)
 
-users ={
+users = {
     "César": "César",
     "Jefferson": "Jefferson Soto Achia",
     "Orlando": "Orlando Flores",
 }
 
 flags = [
-    {'flag': 'B1', 'description': 'Descanso 1'},
-    {'flag': 'B2', 'description': 'Descanso 2'},
-    {'flag': 'LCH', 'description': 'Almuerzo'},
-    {'flag': 'OUT', 'description': 'Salida'},
-    {'flag': 'EMR', 'description': 'Emergencia'},
-    {'flag': 'COR', 'description': 'Corrección'},    
-    {'flag': 'BTW', 'description': 'Back to work'},    
-]              
+    {"flag": "B1", "description": "Descanso 1"},
+    {"flag": "B2", "description": "Descanso 2"},
+    {"flag": "LCH", "description": "Almuerzo"},
+    {"flag": "OUT", "description": "Salida"},
+    {"flag": "EMR", "description": "Emergencia"},
+    {"flag": "COR", "description": "Corrección"},
+    {"flag": "BTW", "description": "Back to work"},
+]
 
 
-flags_regex_str = '|'.join([f['flag'] for f in flags]) 
-flags_regex_str = f'({flags_regex_str})'
+flags_regex_str = "|".join([f["flag"] for f in flags])
+flags_regex_str = f"({flags_regex_str})"
 
-notification_regex = re.compile(r'(\d{1,2}/\d{1,2}/\d{1,2}), (\d{2}:\d{2}) - (.*): (B1|B2|LCH|OUT|EMR|COR|BTW)([\s\S]*)')
+notification_regex = re.compile(
+    r"(\d{1,2}/\d{1,2}/\d{1,2}), (\d{2}:\d{2}) - (.*): (B1|B2|LCH|OUT|EMR|COR|BTW)([\s\S]*)"
+)
 
 file = open(file_path, "r")
 for line in file:
@@ -73,42 +90,79 @@ for line in file:
         user = regex_match.group(3)
         flag = regex_match.group(4)
         message = regex_match.group(5)
-        # message = "mssg"
         message = message.strip()
-        message = message.replace('\n', '')
-        if message == '':
-            message = "N/A"
-        print(f'date: {date} time: {time} user: {user} flag: {flag} message: {message}')
+        message = message.replace("\n", "")
+        # print(f"date: {date} time: {time} user: {user} flag: {flag} message: {message}")
 
-        cursor.execute('''
-            INSERT INTO log(date, time, user, flag, message)
-            VALUES(?, ?, ?, ?, ?)
+        timesatmp = datetime.datetime.strptime(f"{date} {time}", r"%m/%d/%y %H:%M")
+
+        cursor.execute(
+            f"""
+            INSERT INTO log(date, time, timestamp, user, flag, message)
+            VALUES(?, ?, {timesatmp.timestamp()}, ?, ?, ?)
                     ON CONFLICT(date, time, user, flag) 
                     DO 
                        UPDATE SET message = excluded.message
-        ''', (date, time, user, flag, message))
+        """,
+            (date, time, user, flag, message),
+        )
         connection.commit()
 
 file.close()
-#select all data from log table
-res = cursor.execute('''
+res = cursor.execute(
+    """
     SELECT 
-        date AS date,
-        time AS time,
-        user AS user,
-        flag AS flag,
-        message AS message
+        date,
+        time,
+        timestamp,
+        user,
+        flag,
+        message,   
+        puch_date,
+        punch_time,
+        punch_timestamp,
+        is_valid,
+        notes
     FROM log
-''')
+"""
+)
 
 
-#write to csv
-with open(csv_file_path, mode='w', newline='') as file:
-    writer = csv.writer(file, dialect='excel', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['date', 'time', 'user', 'flag', 'message'])
+# write to csv
+with open(csv_file_path, mode="w", newline="") as file:
+    writer = csv.writer(file, dialect="excel")
+    writer.writerow(
+        [
+            "DATE",
+            "TIME",
+            "TIMESTAMP",
+            "USER",
+            "FLAG",
+            "MESSAGE",
+            "PUCH_DATE",
+            "PUNCH_TIME",
+            "PUNCH_TIMESTAMP",
+            "IS_VALID",
+            "NOTES",
+        ]
+    )
     for row in res.fetchall():
-        writer.writerow([row[0], row[1], row[2], row[3], row[4] ])
+        writer.writerow(row)
         # print(row)
 
-connection.close()
+# create table auxiliar
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS auxiliar(
+        auxiliar_id INTEGER PRIMARY KEY,
+        user TEXT,
+        flag TEXT,
+        start_timestamp INTEGER,
+        end_timestamp INTEGER,
+        notes TEXT   
+    )
+"""
+)
 
+
+connection.close()
